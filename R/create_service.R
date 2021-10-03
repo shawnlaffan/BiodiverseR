340d88579541e2ce81283326168cff385f3ad2d2
Jakub Tomczyk <jstomczyk@gmail.com>
2021-10-03 16:06:34 +1100

    Added an example of documentation for the create_service.R function.

--- R/create_service.R
+++ R/create_service.R
@@ -1,3 +1,15 @@
+#' Create service
+#'
+#' Create a service which allows us to access functionalities of the Biodiverse
+#' tool.
+#'
+#' @param test_str string
+#'
+#' @return
+#' @export
+#' @examples
+#' create_service("test")
+
 create_service = function(test_str){
   # Print input to screen
   print(test_str)
