d3117a12085bccf5fe948000e23bdf863d56670a
shawnlaffan <shawnlaffan@gmail.com>
2022-06-14 15:32:39 +1000

    add a one-shot spatial analysis
    All the data and analysis args are passed,
    and a table is returned.

--- perl/lib/BiodiverseR.pm
+++ perl/lib/BiodiverseR.pm
@@ -8,6 +8,10 @@ use lib curfile->dirname->dirname->dirname->dirname->child('biodiverse/lib')->to
 
 use BiodiverseR::Data;
 use Biodiverse::BaseData;
+use Biodiverse::ReadNexus;
+use Biodiverse::Spatial;
+
+local $| = 1;
 
 #use JSON::Validator 5.08 ();
 
@@ -37,37 +41,89 @@ sub startup ($self) {
 
   # Normal route to controller
   $r->get('/')->to('Example#welcome');
-  
 
-  $r->post ('/basedata' => sub {
-    my $c = shift;
-    my $hash = $c->req->json;
+  #  pass some data, get a result.  Or the broken pieces. 
+  $r->post ('/analysis_spatial_oneshot' => sub {
+        my $c = shift;
+    my $analysis_params = $c->req->json;
 use Data::Printer;
-p $hash;
-    my $params = $hash->{params};
-    my $data   = $hash->{data};
+p $analysis_params;
+    my $bd_params = $analysis_params->{bd}{params};
+    my $bd_data   = $analysis_params->{bd}{data};
     my $bd = Biodiverse::BaseData->new(
-      NAME       => ($params->{name} // 'BiodiverseR'),
-      CELL_SIZES => $params->{cellsizes},
+      NAME       => ($bd_params->{name} // 'BiodiverseR'),
+      CELL_SIZES => $bd_params->{cellsizes},
     );
+#p $bd;
+    #  needs to be a more general call
+    my $csv_object = $bd->get_csv_object(
+      quote_char => $bd->get_param('QUOTES'),
+      sep_char   => $bd->get_param('JOIN_CHAR')
+    );
+p $bd_data;
+    eval {
+      $bd->add_elements_collated_simple_aa($bd_data, $csv_object, 1);
+    };
+    croak $@ if $@;
+say STDERR 'aaaargh';
+#p $bd;
+    my $tree;
+    if ($analysis_params->{tree}) {
+      my $readnex = Biodiverse::ReadNexus->new;
+      $readnex->import_data(data => $analysis_params->{tree});
+      my @results = $readnex->get_tree_array;
+      $tree = shift @results;
+    }
+      
     $c->data->{basedata} = $bd;
-    $c->render(json => $bd);
-p $bd;
-    #$c->render(json => {});
-    #my $description = $bd->describe;
-    #Mojo::Log->log($description);
-    #$c->render(json => {description => $description});
-  #} => 'index');
+    $c->data->{tree}     = $tree;
+#p $bd->get_groups_ref;
+#p $tree;
+
+    #  need to be params
+    my $spatial_conditions = ['sp_self_only()'];  
+    my $calculations = ['calc_endemism_central'];
+    my $result_list  = 'SPATIAL_RESULTS';
+    
+    my $sp = $bd->add_spatial_output(name => 'ooyah');
+    $sp->run_analysis (
+      spatial_conditions => $spatial_conditions,
+      calculations => $calculations,
+    );
+    my $table = $sp->to_table (list => $result_list);
+    $c->render(text => $table);
+    p $table;
   });
 
-  $r->get ('/basedata' => sub {
-    my $c = shift;
-    my $bd = $c->data->{basedata};
-    $c->render(json => $bd);
-p $bd;
-    #$c->render(json => {});
-  #} => 'index');
-  });
+#  $r->post ('/basedata' => sub {
+#    my $c = shift;
+#    my $hash = $c->req->json;
+#use Data::Printer;
+#p $hash;
+#    my $params = $hash->{params};
+#    my $data   = $hash->{data};
+#    my $bd = Biodiverse::BaseData->new(
+#      NAME       => ($params->{name} // 'BiodiverseR'),
+#      CELL_SIZES => $params->{cellsizes},
+#    );
+#    $c->data->{basedata} = $bd;
+#    $c->render(json => $bd);
+#p $bd;
+#    #$c->render(json => {});
+#    #my $description = $bd->describe;
+#    #Mojo::Log->log($description);
+#    #$c->render(json => {description => $description});
+#  #} => 'index');
+#  });
+
+#  $r->get ('/basedata' => sub {
+#    my $c = shift;
+#    my $bd = $c->data->{basedata};
+#    $c->render(json => $bd);
+#p $bd;
+#    #$c->render(json => {});
+#  #} => 'index');
+#  });
 
   
 }
--- perl/t/basic.t
+++ perl/t/basic.t
@@ -9,26 +9,32 @@ $t->get_ok('/')->status_is(200)->content_like(qr/Mojolicious/i);
 #$t->get_ok('/results/joel.json')
 #  ->json_is({name => 'Joel Berger', status => 'nice'});
   
-my $ua = Mojo::UserAgent->new;
-$ua->post(
-  'http://127.0.0.1:3000/basedata',
-  {
-    params => {name => 'blognorb', cellsizes => [100,100]},
-    data   => {
-      label1 => '50:50',
-      label1 => '150:50',
-      label2 => '50:50',
-      label2 => '150:50',
-    },
-  },
-  'some binary content'
-);
+#my $ua = Mojo::UserAgent->new;
+#$ua->post(
+#  'http://127.0.0.1:3000/basedata',
+#  {
+#    params => {name => 'blognorb', cellsizes => [100,100]},
+#    data   => {
+#      label1 => '50:50',
+#      label1 => '150:50',
+#      label2 => '50:50',
+#      label2 => '150:50',
+#    },
+#  },
+#  'some binary content'
+#);
 
+my $gp_lb = {
+  '50:50'   => {label1 => 1, label2 => 1},
+  '150:150' => {label1 => 1, label2 => 1},
+};
 my $bd_as_json = {
-  params => {name => 'blognorb', cellsizes => [100,100]},
-  data   => {a => '50:50', b => '150:50'},
+  bd => {
+    params => {name => 'blognorb', cellsizes => [100,100]},
+    data   => $gp_lb,
+  },
 }; 
-$t->post_ok ('/basedata' => json => $bd_as_json)
+$t->post_ok ('/analysis_spatial_oneshot' => json => $bd_as_json)
   ->status_is(200)
   ->json_is(undef);
 #$t->get_ok ('/basedata' => {Accept => '*/*'} => json => {a => 'b'})
