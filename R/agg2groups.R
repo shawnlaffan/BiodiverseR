0c9788032bbfa7f47ac6f4321e37b903857198b7
Aniko Toth <z3531221@ad.unsw.edu.au>
2023-09-11 17:17:49 +1000

    abund_col and ID_col now take vectors. New argument group_col for character attributes also takes a vector. Cellsize takes up to 2 dimensions. Aggregation by attributes and perfect match points is supported.

--- R/agg2groups.R
+++ R/agg2groups.R
@@ -6,9 +6,10 @@
 #' @param x input data as path, data source name to be passed to st_read, data.frame, sf object, or SpatRaster object
 #' @param layer in case of shapefile data format: name of layer to be passed to st_read
 #' @param coords in case of point data: names or numbers of the numeric columns holding coordinates, to be passed to st_as_sf. Required if reading data from .xls file or inputting data.frame
-#' @param abund_col name of column containing abundances; defaults to "count" and is assumed to be 1 for all records if no column specified and count column is absent
-#' @param ID_col name of column containing data labels or species names. In the event of multiple label columns, further names can be passed in using ... parameter
-#' @param res numeric; size of desired groups
+#' @param abund_col character vector containing the name of column(s) with abundances; defaults to "count" and is assumed to be 1 for all records if no column specified and count column is absent
+#' @param ID_col character vector containing the name of column(s) with data labels or species names. Defaults to "label"
+#' @param group_col character vector containing the name of column(s) with attribute data by which to group observations.
+#' @param cellsize numeric vector indicating the size of desired groups in up to 4 dimensions. If 0, exact point matches are aggregated. A negative number will aggregate based on attributes in group_col.
 #' @param origin numeric vector of 2 numbers specifying the x and y dimensions of the spatial groups into which data needs to be aggregated. Units should be the same as the data projection, usually metres.
 #' @param fun name of aggregation function to be used. Defaults to sum.
 #' @param ... passed on to agg2groups call as grouping/ID variables.
@@ -57,38 +58,72 @@ agg2groups.data.frame <- function(x, coords, ...) {
 }
 
 # spatial aggregation, count and label columns specified with convenient defaults
-agg2groups.sf <- function(x, abund_col = count, ID_col = label, res = 100, origin = c(0,0), fun = sum, ...) {
+agg2groups.sf <- function(x, abund_col = c("count"), ID_col = c("label"), group_col, cellsize = 100, origin = c(0,0), fun = sum, ...) {
   # add XY if not present
   if(!all(c("X", "Y") %in% names(x))) x <- data.frame(x, st_coordinates(x))
 
-  abund_col <- enquo(abund_col)
-  ID_col <- enquo(ID_col)
+  #abund_col <- enquo(abund_col)
+  #ID_col <- enquo(ID_col)
 
-  if(!quo_name(abund_col) %in% names(x)){
-    message(paste("Column", quo_name(abund_col), "not found. Defaulting to all counts = 1."))
-    x <- x %>% mutate(!!abund_col := 1)
+  # check for existence of abund_col names
+  if(!all(abund_col %in% names(x))){
+    missing <- abund_col[!abund_col %in% names(x)]
+    if(length(abund_col) > 1){
+      if(length(missing) == length(abund_col)){
+        message("Abundance columns not found. Reverting to default: count = 1 for all rows.")
+        abund_col <- "count"
+        x <- x %>% mutate("{abund_col}" = 1)
+
+      }else{message(paste("Column ", missing[1], " not found. Missing columns are ignored."))
+        abund_col <- abund_col[abund_col %in% names(x)]
+      }
+
+    }
+    if(length(abund_col) == 1){
+      message(paste("Column ", missing, " not found. Defaulting to count = 1."))
+      x <- x %>% mutate("{abund_col}" = 1)
+    }
   }
 
+
   # aggregate and summarise
-  out <- x %>% group_by(x = round_any(X, res, origin = origin[1]),
-                        y = round_any(Y, res, origin = origin[2]),
-                        !!ID_col,
-                        !!! ensym(...)) %>%
-    summarise(value := fun(as.numeric(!!abund_col))) %>% st_drop_geometry()
+  if(length(cellsize) == 1){
+    if(cellsize > 0){
+      out <- x %>% mutate(x = round_any(X, cellsize, origin = origin[1]),
+                          y = round_any(Y, cellsize, origin = origin[2])) %>%
+        group_by(across(c(x, y, all_of(ID_col)))) %>%
+        summarise(value := across(all_of(abund_col), fun)) %>% st_drop_geometry()
+    }
+    if(cellsize == 0 ){
+      out <- x %>% group_by(across(c(x=X, y=Y, all_of(ID_col)))) %>%
+        summarise(value := across(all_of(abund_col), fun)) %>% st_drop_geometry()
+    }
+    if(cellsize < 0){
+      out <- x %>% group_by(all_of(group_col), all_of(ID_col)) %>%
+        summarise(value := across(all_of(abund_col), fun)) %>% st_drop_geometry()
+    }
+  }else if(length(cellsize) == 2) {
+    out <- x %>% group_by(x = round_any(X, cellsize[1], origin = origin[1]),
+                          y = round_any(Y, cellsize[2], origin = origin[2]),
+                          all_of(ID_col)) %>%
+      summarise(value := across(all_of(abund_col), fun)) %>% st_drop_geometry()
+  }
+
 
   # change to format required by json
-  names <- paste(out$x, out$y, sep = ":")
-  out <- out %>% ungroup %>% dplyr::select(-x, -y) %>% split(names) %>%
-    purrr::map(~tidyr::pivot_wider(.x, names_from = c(!!ID_col, !!! ensym(...)), values_from = value) %>% unlist())
+  #names <- paste(out$x, out$y, sep = ":")
+  names <- out %>% select(group_cols()) %>% apply(1, paste, collapse = ":")
+  out <- out %>% split(names) %>%
+    purrr::map(~tidyr::pivot_wider(.x, names_from = all_of(ID_col), values_from = value) %>% unlist())
 
   return(out)
 }
 
 # raster aggregation
-agg2groups.SpatRaster <- function(x, res = 100, ...) {
- temp <- aggregate(x, fact = res/res(x), fun = sum, ...) |>
+agg2groups.SpatRaster <- function(x, cellsize = 100, ...) {
+ temp <- aggregate(x, fact = cellsize/res(x), fun = sum, ...) |>
     as.data.frame(xy = TRUE) |>
-    mutate(x = x - res/2, y = y - res/2) # change xy to represent bottom left corner rather than centre of cell
+    mutate(x = x - cellsize/2, y = y - cellsize/2) # change xy to represent bottom left corner rather than centre of cell
 
   # change data format to json
   names <- paste(temp$x, temp$y, sep = ":")
