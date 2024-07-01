f676428978f1dcb093ea2c138c4df137b30aaaad
Fonti Kar <f.kar@unsw.edu.au>
2024-07-01 12:50:54 +1000

    Added perlbrewr as dependency and started new function for installation on macs

---
+++ R/install_perl_on_mac.R
@@ -0,0 +1,12 @@
+install_perlbrewr <- function(perl_version = "5.36.1"){
+  # Perlbrewr function to tell R to use perlbrew
+
+  # Check if root path exists in environment, if not set it
+  # TODO:Currently not working
+  # if(Sys.getenv("PERLBREW_ROOT") == "")
+  # result <- perlbrewr::perlbrew(root = "~/perl5/perlbrew/bin/perlbrew", version = perl_version)
+  #else
+    result <- perlbrewr::perlbrew(root = Sys.getenv("PERLBREW_ROOT"), version = perl_version)
+
+    return(result)
+}
